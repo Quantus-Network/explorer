5d87167c2cd47ca1d019b561ce71cc6d8e4a4c4e
Dewa Bisma <idewagedebisma@gmail.com>
2025-06-27 12:04:29 +0800

    fix: toggle theme initial toggle state

--- src/components/ui/composites/theme-toggle/ThemeToggle.tsx
+++ src/components/ui/composites/theme-toggle/ThemeToggle.tsx
@@ -9,7 +9,7 @@ import { Skeleton } from '../../skeleton';
 
 export const ThemeToggle = () => {
   const isClient = useIsClient();
-  const { theme, setTheme } = useTheme();
+  const { setTheme, resolvedTheme } = useTheme();
 
   const toggleTheme = (isDark: boolean) => {
     setTheme(isDark ? 'dark' : 'light');
@@ -19,13 +19,13 @@ export const ThemeToggle = () => {
 
   return (
     <Switch.Root
-      checked={theme === 'dark'}
+      checked={resolvedTheme === 'dark'}
       onCheckedChange={toggleTheme}
       className="relative flex h-8 w-16 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input"
     >
       <Switch.Thumb asChild>
         <div className="absolute flex size-7 items-center justify-center rounded-full border bg-secondary shadow-md transition-transform duration-300 ease-in-out data-[state=checked]:translate-x-8">
-          {theme === 'dark' ? <Moon /> : <Sun />}
+          {resolvedTheme === 'dark' ? <Moon /> : <Sun />}
         </div>
       </Switch.Thumb>
     </Switch.Root>
