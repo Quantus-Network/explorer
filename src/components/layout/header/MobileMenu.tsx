dd45299deea3bb0bff06db0817c24a3b7b66a41b
Dewa Bisma <idewagedebisma@gmail.com>
2025-07-10 13:23:10 +0800

    feat: make navbar item grouped for related resource, add type-guard utility, change denominator to 12

--- src/components/layout/header/MobileMenu.tsx
+++ src/components/layout/header/MobileMenu.tsx
@@ -6,12 +6,16 @@ import { ThemeToggle } from '@/components/ui/composites/theme-toggle/ThemeToggle
 import { ContentContainer } from '@/components/ui/content-container';
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList
+  NavigationMenuList,
+  NavigationMenuTrigger
 } from '@/components/ui/navigation-menu';
+import type { ParentNavigation } from '@/config/site-navigations';
 import { SITE_NAVIGATIONS } from '@/config/site-navigations';
 import { cn } from '@/lib/utils';
+import { isInstanceOf } from '@/utils/type-guard';
 
 export interface MobileMenuProps {
   isOpen: boolean;
@@ -28,26 +32,59 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
   return (
     <ContentContainer
       className={cn(
-        'absolute top-16 bg-navbar-mobile-menu transition-opacity duration-300 ease-in-out opacity-100 lg:hidden backdrop-blur-sm',
+        'absolute top-16 bg-navbar-mobile-menu transition-opacity duration-300 ease-in-out opacity-100 md:hidden backdrop-blur-sm',
         !isOpen && 'opacity-0 pointer-events-none'
       )}
     >
-      <NavigationMenu className="items-start">
+      <NavigationMenu className="items-start [&>div+div]:top-[120px]">
         <NavigationMenuList className="flex flex-col items-start gap-8 py-8">
-          {SITE_NAVIGATIONS.map((nav) => (
-            <NavigationMenuItem key={nav.path} className="!ml-0">
-              <NavigationMenuLink asChild>
-                <Link
-                  href={nav.path}
-                  className="no-underline data-[active=true]:font-semibold"
-                  data-active={rootPath === nav.path.split('/')[1]}
-                  onClick={toggleMenu}
-                >
-                  {nav.label}
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
+          {SITE_NAVIGATIONS.map((nav) => {
+            if (isInstanceOf<ParentNavigation>(nav, 'children'))
+              return (
+                <NavigationMenuItem key={nav.label} className="!ml-0">
+                  <NavigationMenuTrigger
+                    className="data-[act ive=true]:font-semibold bg-transparent p-0 text-base no-underline hover:!bg-transparent  focus:!bg-transparent"
+                    data-active={
+                      !!nav.children.find(
+                        (subNav) => subNav.path.split('/')[1] === rootPath
+                      )
+                    }
+                  >
+                    {nav.label}
+                  </NavigationMenuTrigger>
+
+                  <NavigationMenuContent className="flex flex-col gap-2 whitespace-nowrap p-2">
+                    {nav.children.map((subNav) => (
+                      <NavigationMenuLink key={subNav.path} asChild>
+                        <Link
+                          href={subNav.path}
+                          className="no-underline data-[active=true]:font-semibold"
+                          data-active={rootPath === subNav.path.split('/')[1]}
+                          onClick={toggleMenu}
+                        >
+                          {subNav.label}
+                        </Link>
+                      </NavigationMenuLink>
+                    ))}
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              );
+
+            return (
+              <NavigationMenuItem key={nav.path} className="!ml-0">
+                <NavigationMenuLink asChild>
+                  <Link
+                    href={nav.path}
+                    className="no-underline data-[active=true]:font-semibold"
+                    data-active={rootPath === nav.path.split('/')[1]}
+                    onClick={toggleMenu}
+                  >
+                    {nav.label}
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            );
+          })}
 
           <ThemeToggle />
         </NavigationMenuList>
