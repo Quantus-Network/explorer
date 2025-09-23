90502e62099eca985cb2d1e2010498e289d8d00f
Beast <idewagedebisma@gmail.com>
2025-09-23 12:41:03 +0800

    chore: change ticker to QU

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ const env = {
   SITE_SHORT_NAME: 'QE',
   SITE_BASE_URL: import.meta.env.VITE_SITE_URL ?? '',
   GRAPHQL_URL: import.meta.env.VITE_GRAPHQL_URL ?? '',
-  COIN_SYMBOL: 'RES'
+  COIN_SYMBOL: 'QU'
 };
 
 export default env;
